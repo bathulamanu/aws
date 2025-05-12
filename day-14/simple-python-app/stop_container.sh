f410113012eaaf03bfd50280150e459a05dc83b9
bathulamanu <bathulamanohar25@gmail.com>
2025-05-13 02:11:38 +0530

    Update stop_container.sh

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+CONTAINER_NAME="admin-container"
+
+if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+    echo "Stopping and removing existing container..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+else
+    echo "No existing container to stop."
+fi
