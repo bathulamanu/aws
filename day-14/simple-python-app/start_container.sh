2071b4f939c373dfa2b956bd6fcf39654b4e5864
bathulamanu <bathulamanohar25@gmail.com>
2025-05-13 02:11:07 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
-echo
+# Define variables
+IMAGE="495599757751.dkr.ecr.us-east-1.amazonaws.com/admin:latest"
+CONTAINER_NAME="admin-container"
+
+# Login to ECR (make sure EC2 has permission)
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 495599757751.dkr.ecr.us-east-1.amazonaws.com
+
+# Pull the image from ECR
+echo "Pulling Docker image from ECR..."
+docker pull $IMAGE
 
 # Run the Docker image as a container
-echo
+echo "Starting container..."
+docker run -d --name $CONTAINER_NAME -p 80:5000 $IMAGE
