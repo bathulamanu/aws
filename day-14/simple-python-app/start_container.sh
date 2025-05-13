52e243bc9e393bd1334df8b0676e2b5b0c4b1b53
bathulamanu <bathulamanohar25@gmail.com>
2025-05-13 20:18:49 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -4,11 +4,11 @@ set -e
 IMAGE="495599757751.dkr.ecr.us-east-1.amazonaws.com/admin:latest"
 CONTAINER_NAME="admin-container"
 
-echo "Logging into ECR..."
+# Login to ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 495599757751.dkr.ecr.us-east-1.amazonaws.com
 
-echo "Pulling latest image from ECR..."
+# Pull latest image
 docker pull $IMAGE
 
-echo "Starting new container..."
+# Start container
 docker run -d --name $CONTAINER_NAME -p 5000:5000 $IMAGE
